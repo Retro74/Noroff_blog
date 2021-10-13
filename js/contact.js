f132c37269bc3ac75928f04b4ca52d1425a50d64
Roger S. Mikalsen <rmikalsen@hotmail.com>
2021-10-13 12:32:41 +0200

    DevStage1

---
+++ js/contact.js
@@ -0,0 +1,10 @@
+const form_contact = document.getElementById("form_contact")
+const div_formResult = document.getElementById("div_formResult")
+//div_formResult.innerHTML ="sss"
+//form_contact.innerHTML ="Hello"
+
+form_contact.addEventListener("submit", function(event){
+    event.preventDefault()
+    
+
+})
