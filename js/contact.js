aa5b23839dc25d9b565b43e60cda071c5d306096
Roger S. Mikalsen <rmikalsen@hotmail.com>
2021-10-21 10:28:23 +0200

    Part 2

--- js/contact.js
+++ js/contact.js
@@ -1,10 +1,38 @@
-const form_contact = document.getElementById("form_contact")
-const div_formResult = document.getElementById("div_formResult")
-//div_formResult.innerHTML ="sss"
-//form_contact.innerHTML ="Hello"
+// Bør gjøre dette for at intellisense skal fungere for elementene
+const form_contact = document.getElementById("form_contact");
+const div_formResult = document.getElementById("div_formResult");
 
-form_contact.addEventListener("submit", function(event){
-    event.preventDefault()
-    
+const form_Values = {}; //JS Object
+let formErrors = []; //JS Array
 
+//Event for innsending av skjema
+form_contact.addEventListener("submit", function (event) {
+  event.preventDefault()
+  formErrors = []; //Reset prev. errors
+  const formData = new FormData(form_contact);
+  for (const [name, value] of formData) {
+    if (!value) {
+      formErrors.push(
+        document.getElementById(name).getAttribute("data-displayErrorText")
+        )
+      document.getElementById("lbl_for_" + name).className = "alert alert-danger";
+    } else {
+      form_Values[name] = value;
+      document.getElementById("lbl_for_" + name).className = "";
+    }
+  }
+
+  if (formErrors.length) {
+    div_formResult.innerHTML = "";
+    for (eachError of formErrors) {
+      div_formResult.innerHTML += "* Feil: " + eachError + "<br>"
+      div_formResult.className = "alert alert-danger"
+    }
+  } else {
+    div_formResult.innerHTML = `
+            <h4>Suksess</h4>
+            <p>Melding sendt</p>
+            `
+    div_formResult.className = "alert alert-success"
+  }
 })
